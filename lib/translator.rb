ad064d1ceec1efe24a18bf61f1187397c2818548
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 03:56:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(path, emoticon)
   result
 end
 
-def get_english_meaning()
+def get_english_meaning(path, emoticon)
   emoticon_hash = load_library(path)
   result = emoticon_hash["get_meaning"][emoticon]
   if result == nil
